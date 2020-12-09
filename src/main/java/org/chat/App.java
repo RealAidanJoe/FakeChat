0faa10277a562306170678a844e2981b056da647
nosnakeob.surface <e01814034@stu.ahu.edu.cn>
2020-12-09 16:44:45 +0800

    完善界面

--- src/main/java/org/chat/App.java
+++ src/main/java/org/chat/App.java
@@ -21,7 +21,7 @@ public class App extends Application {
         scene = new Scene(loadFXML("home"));
         stage.setScene(scene);
         stage.setTitle("FakeChat");
-        stage.initStyle(StageStyle.TRANSPARENT);
+//        stage.initStyle(StageStyle.TRANSPARENT);
         stage.show();
     }
 
@@ -29,7 +29,7 @@ public class App extends Application {
         scene.setRoot(loadFXML(fxml));
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
+    public static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
--- src/main/java/org/chat/controller/Home.java
+++ src/main/java/org/chat/controller/Home.java
@@ -1,4 +1,28 @@
 package org.chat.controller;
 
-public class Home {
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import org.chat.App;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Home implements Initializable {
+    public VBox userList;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Parent node = null;
+        try {
+            node = App.loadFXML("friend");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        userList.getChildren().add(node);
+    }
 }
